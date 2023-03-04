4346a307f806729ddab1d33a317dd7cd4cf5f585
Iedo <iedo2298@gmail.com>
2023-03-03 21:04:35 -0300

    aaaaa

--- ExSemana/src/modules/dom.js
+++ ExSemana/src/modules/dom.js
@@ -1,11 +1,17 @@
-import filmes from './cadastros.js';
+import filmes, { indexPorTitulo } from "./cadastros.js";
 
-export function gerarPatioDeCards(classeCard="", idPatio="", listaCards=[]){
-    document.getElementById(idPatio).innerHTML ="";
-    listaCards.forEach((card)=> {
-            document.getElementById(idPatio).innerHTML += `
+export function gerarPatioDeCards(
+  classeCard = "",
+  idPatio = "",
+  listaCards = []
+) {
+  document.getElementById(idPatio).innerHTML = "";
+  listaCards.forEach((card) => {
+    document.getElementById(idPatio).innerHTML += `
             <div class="${classeCard}">
-            <img src="${favorito(card.favorito)}" id="${card.titulo}" class="icones">
+            <img src="${favorito(card.favorito)}" id="${
+      card.titulo
+    }" class="icones">
             <img src="" alt="${card.titulo}">
             <ul>
             <li><h2>${card.titulo}</h2></li>
@@ -13,20 +19,23 @@ export function gerarPatioDeCards(classeCard="", idPatio="", listaCards=[]){
             <li>${card.nota}</li>
         </ul>
             </div>`;
-        var elemento = document.getElementById(`${card.titulo}`);
-        
-        elemento.addEventListener("click", () => {           
-            filmes.find(f => f.titulo == `${card.titulo}` ).favoritar();
-            if( filmes.find(f => f.titulo == `${card.titulo}` ).favorito){
-                alert("Favorito");
-      }else{
+
+    var elemento = document.getElementById(card.titulo);
+    elemento.addEventListener("click", () => {
+      filmes.find((f) => f.titulo == `${card.titulo}`).favoritar();
+      if (filmes[indexPorTitulo(`${card.titulo}`, filmes)].favorito) {
+        alert("Favorito");
+      } else {
         alert("Não favorito");
-      };
-      gerarPatioDeCards("filme","catalogo", filmes);})}
-    )}
-export function favorito (favorito)
-{if(favorito){
-    return `./img/favicon.ico`
-    }else{
-    return `./img/favicon (1).ico`
-    }};
+      }
+      gerarPatioDeCards("filme", "catalogo", filmes);
+    });
+  });
+}
+export function favorito(favorito) {
+  if (favorito) {
+    return `./img/favicon.ico`;
+  } else {
+    return `./img/favicon (1).ico`;
+  }
+}
